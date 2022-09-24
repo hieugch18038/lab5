7d4b18bd6974d66e22943a990fdc3d526ca92503
Nguyen Huy Hieu <hieunhgch18038@gmail.com>
2022-09-24 17:06:47 +0700

    g

--- lab5/index.js
+++ lab5/index.js
@@ -8,11 +8,23 @@ const path  = require("path");
 const port = process.env.PORT ||3000;
 
 //set thu muc chuaw view
-app.set("view", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 //set view engine (template engine)
 //=> giup go code cua back-end ngay trong front-end de hon
 //hbs: handle bar => neu dung view engine khac thi thay doi gia tri nay
 app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.get("/mobile", (req, res) => {
+    res.render("mobile");
+})
+
+app.get("/laptop", (req, res) => {
+    res.render("laptop");
+})
 
 //listen port de chay server
 app.listen(port)
